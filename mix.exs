1e86c15f72ee80933be9950c9fadc718a723d99f
Michael Niepel <michael.niepel@gmail.com>
2017-01-06 13:31:51 +0100

    get deps

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Graphql.Mixfile do
   def application do
     [mod: {Graphql, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :absinthe, :absinthe_plug, :absinthe_ecto, :poison, :faker]]
   end
 
   # Specifies which paths to compile per environment.
@@ -37,7 +37,12 @@ defmodule Graphql.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:absinthe, "~> 1.2.0"},
+     {:absinthe_plug, "~> 1.1"},
+     {:absinthe_ecto, git: "https://github.com/absinthe-graphql/absinthe_ecto.git"},
+     {:poison, "~> 2.1.0"},
+     {:faker, "~> 0.7"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
