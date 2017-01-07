08bf967e0fa8108b1edb44a18622c5b17604396d
Michael Niepel <michael.niepel@gmail.com>
2017-01-07 16:45:57 +0100

    elm begginer program

--- web/elm/Main.elm
+++ web/elm/Main.elm
@@ -1,8 +1,62 @@
 module Main exposing (..)
 
-import Html
+import Html exposing (..)
+import Html.Events exposing (onClick)
 
 
-main : Html.Html msg
+-- MODEL
+
+
+type alias Model =
+    { counter : Int
+    }
+
+
+init : Model
+init =
+    Model 0
+
+
+type Msg
+    = Inc
+    | Dec
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Inc ->
+            { model | counter = model.counter + 1 }
+
+        Dec ->
+            { model | counter = model.counter - 1 }
+
+
+
+-- VIEW
+
+
+mainView : Model -> Html Msg
+mainView model =
+    div []
+        [ button [ onClick Inc ] [ text "+" ]
+        , button [ onClick Dec ] [ text "-" ]
+        , p [] [ text (toString model.counter) ]
+        ]
+
+
+
+-- PROGRAM
+
+
+main : Program Never Model Msg
 main =
-    Html.text "Hello from elm!!"
+    Html.beginnerProgram
+        { model = init
+        , view = mainView
+        , update = update
+        }
